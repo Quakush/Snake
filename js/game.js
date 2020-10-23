80aec24e7d80f05668a3e0efdfe38f8a58d73dc9
quakush <quakush@mail.ru>
2020-10-23 10:14:00 +0700

    board

--- js/game.js
+++ js/game.js
@@ -2,14 +2,21 @@ let game = {
 
     canvas: null,
     ctx: null,
+    board: null,
+    width: 640,
+    height: 360,
     sprites: {
         background: null,
         cell: null
     },
-                                                
-    start() {
+
+    init() {
         this.canvas = document.querySelector('#mycanvas'); 
-        this.ctx = this.canvas.getContext('2d');                
+        this.ctx = this.canvas.getContext('2d');
+    },
+                                                
+    start() {                
+        this.init();
 
         this.preload(() => {       
             this.run();
@@ -18,7 +25,7 @@ let game = {
 
     preload(callback) {
         let loaded = 0;
-        let required = 2;
+        let required = Object.keys(this.sprites).length;
         let onAssetLoad = () => {
             ++loaded;
 
@@ -34,9 +41,11 @@ let game = {
     },
 
     run() {
+        this.board.create();
+
         window.requestAnimationFrame( () => {          
             this.ctx.drawImage(this.sprites.background, 0, 0); 
-            this.ctx.drawImage(this.sprites.cell, 320, 180);          
+            this.board.render();          
         });
     }
 };
